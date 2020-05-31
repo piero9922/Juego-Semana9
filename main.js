d8af68c508455df6cf2c6c393f35f3895ea5d9d5
Piero Montalvo <piero.montalvo9922@gmail.com>
2020-05-30 20:34:16 -0500

    Juego Terminado

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 let seguir = "s";
-let resultado = 0;
-
+let resultado = 5;
 function getRandomArbitrary(min, max, apuesta, numeroApuesta) {
+  console.log(apuesta, numeroApuesta);
+  if (isNaN(apuesta) === false && isNaN(numeroApuesta) === false) {
     const numero = Math.random() * (max - min) + min;
-    const numeroAleatorio = parseInt(numero)
-    alert(numeroAleatorio)
-    if(numeroAleatorio == numeroApuesta){
-        resultado = apuesta * 2 + resultado
-    }else {
-        resultado =  apuesta * 0 
-    }
-    alert(`El resultado de tu apuesta es ${resultado}`)
-    
-    seguir = prompt('¿Desea continuar? (s/n)')
-    
-}
-do{
-    let valorApuesta = Number(prompt('¿Cuanto dinero desea apostar? (MAYOR A 5 SOLES)'))
-    let nuevoNumeroApuesta = Number(prompt('Coloque un numero al cual apostar (1 - 6)'))
-    getRandomArbitrary(1,7, valorApuesta, nuevoNumeroApuesta)
+    const numeroAleatorio = parseInt(numero);
+    alert(`EL numero aleatorio es ${numeroAleatorio}`);
 
-}while(seguir == "s")
+    if (numeroAleatorio == numeroApuesta) {
+      alert(
+        `Hasta el momento vas ganando ${(resultado =
+          apuesta * 2 + resultado)} soles`
+      );
+    } else {
+      alert(`Has perdido.Tienes ${(resultado = apuesta * 0)} soles`);
+    }
+
+    seguir = prompt("¿Desea continuar? (s/n)");
+  } 
+  else {
+    alert("Los valores a ingresar tienen que ser un Numeros");
+  }
+}
+do {
+  let valorApuesta = Number(prompt("¿Cuanto dinero desea apostar?"));
+  let nuevoNumeroApuesta = Number(
+    prompt("Coloque un numero al cual apostar (1 - 6)")
+  );
+  getRandomArbitrary(1, 7, valorApuesta, nuevoNumeroApuesta);
+} while (seguir == "s");
